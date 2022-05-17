662eaf17efba2b812c64c5acb80b26a73d0c1e84
Matt Williams <matt@ezpzweb.ca>
2022-05-18 00:46:10 +0900

    use async/await and deep uncle checks

--- src/utils.ts
+++ src/utils.ts
@@ -9,18 +9,22 @@ export enum Chain {
 
 const web3 = new Web3('http://localhost:8545');
 
+const getUncle = async (chain: Chain, blockHeight: number, idx: number, hash: string) => {
+    const block = await web3.eth.getUncle(blockHeight, idx);
+    return hash === block.hash || hash === block.miner;
+};
+
 export const isMinedUncleBlock = async (chain: Chain, blockHeight: number, blockhash?: string, coinbaseAddr?: string): Promise<boolean> => {
     const checksumCoinbaseAddr = web3.utils.toChecksumAddress(coinbaseAddr);
 
-    return web3.eth.getBlock(blockHeight)
-        .then(block => {
-            if (!block) throw new Error(`${chain} at block height ${blockHeight} does not exist`)
-            return block;
-        })
-        .then(block => {
-            if (blockhash && block.hash === blockhash) return false; // if the block at this height and the block hash match, not an uncle
-            if (coinbaseAddr && block.miner === checksumCoinbaseAddr) return false;
-            if (block.uncles.length > 0) return true;
-            throw new Error(`${chain} at block height ${blockHeight} with hash ${blockhash} does not exist`);
-        });
+    const block = await web3.eth.getBlock(blockHeight);
+
+    if (!block) throw new Error(`${chain} at block height ${blockHeight} does not exist`)
+    if (blockhash && block.hash === blockhash) return false; // if the block at this height and the block hash match, not an uncle
+    if (checksumCoinbaseAddr && block.miner === checksumCoinbaseAddr) return false;
+    const isInUncles = block.uncles.reduce(async (acc, ele, idx) => {
+        acc = await getUncle(chain, blockHeight, idx, blockhash || checksumCoinbaseAddr);
+    }, false);
+    if (isInUncles) return true;
+    throw new Error(`${chain} at block height ${blockHeight} with hash ${blockhash} does not exist`);
 };
