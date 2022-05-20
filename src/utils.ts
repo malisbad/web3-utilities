04aa6c9407ad21cc64743932d7e9331b89ade456
Matt Williams <matt@ezpzweb.ca>
2022-05-21 00:49:24 +0900

    Add method for calculating block reward

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,7 @@
 const Web3 = require('web3'); // necessary for Node.js
 
+import BN from "bn.js";
+
 const cliArgs = process.argv.slice(2);
 const provider = cliArgs[0];
 
@@ -7,11 +9,16 @@ export enum Chain {
     ETH = 'ETH',
 }
 
-const web3 = new Web3('http://localhost:8545');
+const web3 = new Web3('http://localhost:8545' || provider);
 
 const getUncle = async (chain: Chain, blockHeight: number, idx: number, hash: string) => {
     const block = await web3.eth.getUncle(blockHeight, idx);
     return hash === block.hash || hash === block.miner;
+};
+
+const getTransactionReceipt = async (txHash: number) => {
+    const tx = await web3.eth.getTransactionReceipt(txHash);
+    return tx;
 };
 
 export const isMinedUncleBlock = async (chain: Chain, blockHeight: number, blockhash?: string, coinbaseAddr?: string): Promise<boolean | Error> => {
@@ -34,3 +41,27 @@ export const isMinedUncleBlock = async (chain: Chain, blockHeight: number, block
         return isUncle;
     };
 };
+
+export const calculateBlockReward = async (blockHeight: number) => {
+    const block = await web3.eth.getBlock(blockHeight);
+    const baseReward = web3.utils.toWei(new BN(2), 'ether');
+    const gasUsed = new BN(block.gasUsed);
+    const baseGasFee = new BN(block.baseFeePerGas);
+    const burnedFee = gasUsed.mul(baseGasFee);
+
+    // TODO this method is slow AF (39s in testing M1 Mac Mini), needs improvement
+    let totalFees = new BN(0);
+    for (let i = 0; i < block.transactions.length; i++) {
+        const txDetails = await getTransactionReceipt(block.transactions[i]);
+        const gasUsed = new BN(txDetails.gasUsed);
+        const gasPrice = new BN(txDetails.effectiveGasPrice);
+        totalFees = totalFees.add(gasUsed.mul(gasPrice));
+    };
+
+    return baseReward.add(await totalFees).sub(burnedFee);
+}
+
+// TODO calculate blockrewards for normal blocks between two dates
+
+// TODO calculate blockrewards for uncle blocks between two dates
+
