2417faa00f708e95d906471978a1653c6e442622
Matt Williams <matt@ezpzweb.ca>
2022-06-20 23:18:50 +0900

    Correctly calculates values with more than one uncle

--- src/rewards.spec.ts
+++ src/rewards.spec.ts
@@ -13,9 +13,9 @@ describe('Block reward calculations', ()  => {
         expect(totalReward.eq(new BN("2195065261011318564"))).toBe(true);
     }, 35000);
 
-    // test('Calculates the correct value for blocks with two uncles', async () => {
-    //     const totalReward = await calculateBlockReward(14992912)
-    //         .catch(console.log);
-    //     expect(totalReward.eq(new BN("2205012962660425231"))).toBe(true);
-    // }, 35000);
+    test('Calculates the correct value for blocks with two uncles', async () => {
+        const totalReward = await calculateBlockReward(14996758)
+            .catch(console.log);
+        expect(totalReward.eq(new BN("2197429983749541284"))).toBe(true);
+    }, 35000);
 });
