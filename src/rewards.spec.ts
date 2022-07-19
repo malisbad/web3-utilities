6b8d566e40003a4bd40d9b6eeef44375e5159a5f
Matt Williams <matt@ezpzweb.ca>
2022-07-19 13:20:58 +0900

    Removed a forgotten debug statement

--- src/rewards.spec.ts
+++ src/rewards.spec.ts
@@ -9,7 +9,7 @@ import { calculateBlockReward } from './utils';
 
 describe('Block reward calculations', ()  => {
     test('Calculates the correct post-London blockreward', async () => {
-        const totalReward = await calculateBlockReward(14811417).then(res => {console.log(res.toString()); return res});
+        const totalReward = await calculateBlockReward(14811417);
         expect(totalReward.eq(new BN("2029985731605798020"))).toBe(true);
     }, 350000);
 
