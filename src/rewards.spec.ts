36b7fae8f28a7806ee0345e357b955fb914b7d85
Matt Williams <matt@ezpzweb.ca>
2022-07-26 11:38:56 +0900

    updated to use class exports and instantiate the package locally

--- src/rewards.spec.ts
+++ src/rewards.spec.ts
@@ -1,20 +1,23 @@
 import BN from 'bn.js';
-import { Chain, calculateBlockReward } from './utils';
+import W3Utils, { Chain } from './utils';
 
 describe('Block reward calculations', ()  => {
+    const chain = Chain.ETH;
+    const w3Utils = new W3Utils("http://localhost:8545");
+
     test('Calculates the correct post-London blockreward', async () => {
-        const totalReward = await calculateBlockReward(14811417);
+        const totalReward = await w3Utils.calculateBlockReward(14811417);
         expect(totalReward.eq(new BN("2029985731605798020"))).toBe(true);
     }, 350000);
 
     test('Calculates the correct value for blocks with one uncle', async () => {
-        const totalReward = await calculateBlockReward(14811419)
+        const totalReward = await w3Utils.calculateBlockReward(14811419)
             .catch(console.log);
         expect(totalReward.eq(new BN("2195065261011318564"))).toBe(true);
     }, 35000);
 
     test('Calculates the correct value for blocks with two uncles', async () => {
-        const totalReward = await calculateBlockReward(14996758)
+        const totalReward = await w3Utils.calculateBlockReward(14996758)
             .catch(console.log);
         expect(totalReward.eq(new BN("2197429983749541284"))).toBe(true);
     }, 35000);
--- src/utils.ts
+++ src/utils.ts
@@ -2,72 +2,76 @@ const Web3 = require('web3'); // necessary for Node.js
 
 import BN from "bn.js";
 
-const cliArgs = process.argv.slice(2);
-const provider = cliArgs[0] || 'http://localhost:8545';
-
 export enum Chain {
     ETH = 'ETH',
 }
 
-let web3 = new Web3(provider);
+export default class W3Utils {
+    web3;
+    subscribe;
+    unsubscribe;
 
-export const subscribe = web3.eth.subcribe;
-
-export const unsubscribe = web3.eth.clearSubscriptions;
-
-const getUncle = async (chain: Chain, blockHeight: number, idx: number, hash: string) => {
-    const block = await web3.eth.getUncle(blockHeight, idx);
-    return hash === block.hash || hash === block.miner;
-};
-
-export const isMinedUncleBlock = async (chain: Chain, blockHeight: number, blockhash?: string, coinbaseAddr?: string): Promise<boolean | Error> => {
-    const checksumCoinbaseAddr = web3.utils.toChecksumAddress(coinbaseAddr);
-    const block = await web3.eth.getBlock(blockHeight);
-
-    if (!block) throw new Error(`${chain} at block height ${blockHeight} does not exist`)
-    if (blockhash && block.hash === blockhash) return false; // if the block at this height and the block hash match, not an uncle
-    if (checksumCoinbaseAddr && block.miner === checksumCoinbaseAddr) return false;
-    if (block.uncles.length === 0) throw new Error(`${chain} at ${blockHeight} does not match and there are no uncles`);
-
-    // deep check the uncles array
-    let isUncle = false;
-    for (let i = 0; i < block.uncles.length; i++) {
-        isUncle = await getUncle(chain, blockHeight, i, blockhash || checksumCoinbaseAddr);
+    constructor(provider: string) {
+        this.web3 = new Web3(provider);
+        this.subscribe = this.web3.subscribe;
+        this.unsubscribe = this.web3.unsubscribe;
     }
-    if (await isUncle === false) {
-        throw new Error(`${chain} at ${blockHeight} is possibly orphaned`)
-    } else {
-        return isUncle;
-    };
-};
 
-export const calculateBlockReward = async (blockHeight: number) => {
-    const block = await web3.eth.getBlock(blockHeight);
-    const baseReward = web3.utils.toWei(new BN(2), 'ether');
-    const gasUsed = new BN(block.gasUsed);
-    const baseGasFee = new BN(block.baseFeePerGas);
-    const uncleInclusionRewards = new BN(web3.utils.toWei("0.0625", 'ether')).mul(new BN(block.uncles.length));
-    const burnedFee = gasUsed.mul(baseGasFee);
 
-    let transactionReceipts: Promise<any>[] = [];
-    for (let i = 0; i < block.transactions.length; i++) {
-        transactionReceipts.push(web3.eth.getTransactionReceipt(block.transactions[i]));
+    async getUncle(chain: Chain, blockHeight: number, idx: number, hash: string) {
+        const block = await this.web3.eth.getUncle(blockHeight, idx);
+        return hash === block.hash || hash === block.miner;
     };
+
+    async isMinedUncleBlock(chain: Chain, blockHeight: number, blockhash?: string, coinbaseAddr?: string): Promise<boolean | Error> {
+        const checksumCoinbaseAddr = this.web3.utils.toChecksumAddress(coinbaseAddr);
+        const block = await this.web3.eth.getBlock(blockHeight);
     
-    // get all of the receipts FIRST, then process otherwise it is too slow
-    return Promise.all(transactionReceipts)
-        .then(receipts => {
-            // can't declare this in the initil value arg as it will always rest to zero
-            let accumulator = new BN(0); 
-            return receipts.reduce((acc, receipt) => {
-                const txGasUsed = new BN(receipt.gasUsed);
-                const gasPrice = new BN(receipt.effectiveGasPrice);
-                const total = txGasUsed.mul(gasPrice)
-                return acc.add(total);
-                }, accumulator
-            )
-        })
-        .then(transactionRewards => baseReward.add(transactionRewards).add(uncleInclusionRewards).sub(burnedFee))
+        if (!block) throw new Error(`${chain} at block height ${blockHeight} does not exist`)
+        if (blockhash && block.hash === blockhash) return false; // if the block at this height and the block hash match, not an uncle
+        if (checksumCoinbaseAddr && block.miner === checksumCoinbaseAddr) return false;
+        if (block.uncles.length === 0) throw new Error(`${chain} at ${blockHeight} does not match and there are no uncles`);
+    
+        // deep check the uncles array
+        let isUncle = false;
+        for (let i = 0; i < block.uncles.length; i++) {
+            isUncle = await this.getUncle(chain, blockHeight, i, blockhash || checksumCoinbaseAddr);
+        }
+        if (await isUncle === false) {
+            throw new Error(`${chain} at ${blockHeight} is possibly orphaned`)
+        } else {
+            return isUncle;
+        };
+    };
+
+    async calculateBlockReward(blockHeight: number) {
+        const block = await this.web3.eth.getBlock(blockHeight);
+        const baseReward = this.web3.utils.toWei(new BN(2), 'ether');
+        const gasUsed = new BN(block.gasUsed);
+        const baseGasFee = new BN(block.baseFeePerGas);
+        const uncleInclusionRewards = new BN(this.web3.utils.toWei("0.0625", 'ether')).mul(new BN(block.uncles.length));
+        const burnedFee = gasUsed.mul(baseGasFee);
+    
+        let transactionReceipts: Promise<any>[] = [];
+        for (let i = 0; i < block.transactions.length; i++) {
+            transactionReceipts.push(this.web3.eth.getTransactionReceipt(block.transactions[i]));
+        };
+        
+        // get all of the receipts FIRST, then process otherwise it is too slow
+        return Promise.all(transactionReceipts)
+            .then(receipts => {
+                // can't declare this in the initil value arg as it will always rest to zero
+                let accumulator = new BN(0); 
+                return receipts.reduce((acc, receipt) => {
+                    const txGasUsed = new BN(receipt.gasUsed);
+                    const gasPrice = new BN(receipt.effectiveGasPrice);
+                    const total = txGasUsed.mul(gasPrice)
+                    return acc.add(total);
+                    }, accumulator
+                )
+            })
+            .then(transactionRewards => baseReward.add(transactionRewards).add(uncleInclusionRewards).sub(burnedFee))
+    }; 
 }
 
 // TODO calculate blockrewards for normal blocks between two dates
